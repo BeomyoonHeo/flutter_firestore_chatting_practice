b4975ef5fe45a4560141adad56286081386dec4f
BeomYoonHeo <gqd0512@naver.com>
2022-12-02 05:01:02 +0900

    메인앱 세팅 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_firestore_chatting_practice/pages/splash_page.dart';
+import 'package:flutter_firestore_chatting_practice/services/navigation_services.dart';
 
 void main() {
   runApp(SplashPage(
     key: UniqueKey(),
     onInitalizationComplete: () {},
   ));
+}
+
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GgamfChat',
+      theme: ThemeData(
+        backgroundColor: const Color.fromRGBO(36, 35, 49, 1.0),
+        scaffoldBackgroundColor: const Color.fromRGBO(36, 35, 49, 1.0),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: const Color.fromRGBO(30, 29, 37, 1.0),
+        ),
+      ),
+      navigatorKey: NavigationService.navigatorKey,
+    );
+  }
 }
