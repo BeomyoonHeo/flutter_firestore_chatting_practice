c7060b368f71282e40a02354fda6cc9a3be84731
BeomyoonHeo <55969155+BeomyoonHeo@users.noreply.github.com>
2022-12-02 12:06:08 +0900

    Splash Page 완료
    * firebase_analytics 의존 추가
    
    * splash 페이지 구성 완료
    
    * 어플 접속시 에널리틱스 확인 완료
    
    * 네비게이션 서비스 생성 완료
    
    * 미디어 서비스 생성 완료
    
    * 클라우드 스토리지 서비스 생성 완료
    
    * 데이터베이스 서비스 생성 완료
    
    * 메인앱 세팅 완료
    
    * 네비게이션 키 에러 수정 및 페이지 딜레이 추가

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,78 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_firestore_chatting_practice/services/cloud_storage_service.dart';
+import 'package:flutter_firestore_chatting_practice/services/database_service.dart';
+import 'package:flutter_firestore_chatting_practice/services/media_service.dart';
+import 'package:flutter_firestore_chatting_practice/services/navigation_services.dart';
+import 'package:get_it/get_it.dart';
+
+class SplashPage extends StatefulWidget {
+  final VoidCallback onInitalizationComplete;
+  const SplashPage({required Key key, required this.onInitalizationComplete})
+      : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 1)).then(
+      (_) {
+        _setup().then(
+          (_) => widget.onInitalizationComplete(),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GamfChat',
+      theme: ThemeData(
+        backgroundColor: const Color.fromRGBO(36, 35, 49, 1.0),
+        scaffoldBackgroundColor: const Color.fromRGBO(36, 35, 49, 1.0),
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+              fit: BoxFit.contain,
+              image: AssetImage('assets/images/logo.png'),
+            )),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _setup() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    _registerServices();
+  }
+
+  void _registerServices() {
+    GetIt.instance.registerSingleton<NavigationService>(
+      NavigationService(),
+    );
+
+    GetIt.instance.registerSingleton<MediaService>(
+      MediaService(),
+    );
+
+    GetIt.instance.registerSingleton<CloudStorageService>(
+      CloudStorageService(),
+    );
+
+    GetIt.instance.registerSingleton<DatabaseService>(
+      DatabaseService(),
+    );
+  }
+}
