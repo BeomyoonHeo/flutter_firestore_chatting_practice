e40993f77493fdfe9e09e8e9944edf1ed4c2ba1a
BeomyoonHeo <55969155+BeomyoonHeo@users.noreply.github.com>
2022-12-02 12:12:59 +0900

    로그인 페이지 구성 완료
    * firebase_analytics 의존 추가
    
    * splash 페이지 구성 완료
    
    * 어플 접속시 에널리틱스 확인 완료
    
    * 네비게이션 서비스 생성 완료
    
    * 미디어 서비스 생성 완료
    
    * 클라우드 스토리지 서비스 생성 완료
    
    * 데이터베이스 서비스 생성 완료
    
    * 메인앱 세팅 완료
    
    * 네비게이션 키 에러 수정 및 페이지 딜레이 추가
    
    * 로그인 페이지 생성 및 네비게이터 키 라우트 완료
    
    * 로그인페이지 뼈대 구성 및 타이틀 만들기 완료
    
    * 위젯 컴포넌트 화 및 텍스트 필드 구성 완료
    
    * 로그인 버튼 구성 완료 및 컴포넌트 화 완료
    
    * 어카운트 링크 생성 완료

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -7,7 +7,7 @@ import 'package:flutter_firestore_chatting_practice/services/navigation_services
 import 'package:get_it/get_it.dart';
 
 class SplashPage extends StatefulWidget {
-  final VoidCallback onInitalizationComplete;
+  final VoidCallback onInitalizationComplete; // 익명함수를 사용 할 수 있게 해주는 자료형
   const SplashPage({required Key key, required this.onInitalizationComplete})
       : super(key: key);
 
@@ -19,7 +19,8 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(seconds: 1)).then(
+    //Splash 페이지가 작동하는 것을 확인하기 위해서 딜레이 1초 주고 initialize 시킴
+    Future.delayed(const Duration(seconds: 1)).then(
       (_) {
         _setup().then(
           (_) => widget.onInitalizationComplete(),
@@ -41,7 +42,7 @@ class _SplashPageState extends State<SplashPage> {
           child: Container(
             height: 200,
             width: 200,
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
                 image: DecorationImage(
               fit: BoxFit.contain,
               image: AssetImage('assets/images/logo.png'),
@@ -52,6 +53,8 @@ class _SplashPageState extends State<SplashPage> {
     );
   }
 
+  //파이어베이스와의 연동이 완료 될 경우 get라이브러리의 싱글턴 패턴을 통하여
+  //모든 서비스를 컨테이너에 띄워놓음 각 서비스는 라이브러리를 의존하여 서비스 진행
   Future<void> _setup() async {
     WidgetsFlutterBinding.ensureInitialized();
     await Firebase.initializeApp();
