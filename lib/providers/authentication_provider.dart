94da7a21cccfd13888e00cc95508da9b3026ad7f
BeomYoonHeo <gqd0512@naver.com>
2022-12-08 21:54:00 +0900

    유저정보 가져오기 완료

--- lib/providers/authentication_provider.dart
+++ lib/providers/authentication_provider.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firestore_chatting_practice/models/chat_user.dart';
 import 'package:flutter_firestore_chatting_practice/services/database_service.dart';
 import 'package:flutter_firestore_chatting_practice/services/navigation_services.dart';
 import 'package:get_it/get_it.dart';
@@ -8,6 +9,9 @@ class AuthenticationProvider extends ChangeNotifier {
   late final FirebaseAuth _auth;
   late final NavigationService _navigationService;
   late final DatabaseService _databaseService;
+
+  late ChatUser user;
+
   AuthenticationProvider() {
     _auth = FirebaseAuth.instance;
     _navigationService = GetIt.instance.get<NavigationService>();
@@ -16,7 +20,22 @@ class AuthenticationProvider extends ChangeNotifier {
     _auth.authStateChanges().listen((_user) {
       if (_user != null) {
         _databaseService.updateUserLastSeenTime(_user.uid);
-        _databaseService.getUser(_user.uid);
+        _databaseService.getUser(_user.uid).then(
+          (_snapshot) {
+            Map<String, dynamic> _userData =
+                _snapshot.data()! as Map<String, dynamic>;
+            user = ChatUser.fromJSON(
+              {
+                'uid': _user.uid,
+                'name': _userData['name'],
+                'email': _userData['email'],
+                'last_active': _userData['last_active'],
+                'image': _userData['image'],
+              },
+            );
+            print(user.toMap());
+          },
+        );
       } else {
         print('Not Authenticated');
       }
