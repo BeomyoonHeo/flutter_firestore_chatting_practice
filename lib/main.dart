3ca8fc49f3499c400b53ca5281517cbb5b21ba5f
BeomYoonHeo <gqd0512@naver.com>
2022-12-08 19:42:22 +0900

    provider 등록 완료

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_firestore_chatting_practice/pages/login_page.dart';
 
 import 'package:flutter_firestore_chatting_practice/pages/splash_page.dart';
+import 'package:flutter_firestore_chatting_practice/providers/authentication.dart';
 import 'package:flutter_firestore_chatting_practice/services/navigation_services.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(SplashPage(
@@ -18,20 +20,29 @@ void main() {
 class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'GgamfChat',
-      theme: ThemeData(
-        backgroundColor: const Color.fromRGBO(36, 35, 49, 1.0),
-        scaffoldBackgroundColor: const Color.fromRGBO(36, 35, 49, 1.0),
-        bottomNavigationBarTheme: BottomNavigationBarThemeData(
-          backgroundColor: const Color.fromRGBO(30, 29, 37, 1.0),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthenticationProvider>(
+          create: (context) {
+            return AuthenticationProvider();
+          },
+        )
+      ],
+      child: MaterialApp(
+        title: 'GgamfChat',
+        theme: ThemeData(
+          backgroundColor: const Color.fromRGBO(36, 35, 49, 1.0),
+          scaffoldBackgroundColor: const Color.fromRGBO(36, 35, 49, 1.0),
+          bottomNavigationBarTheme: BottomNavigationBarThemeData(
+            backgroundColor: const Color.fromRGBO(30, 29, 37, 1.0),
+          ),
         ),
+        navigatorKey: NavigationService.navigatorKey,
+        initialRoute: '/login',
+        routes: {
+          '/login': (BuildContext context) => LoginPage(),
+        },
       ),
-      navigatorKey: NavigationService.navigatorKey,
-      initialRoute: '/login',
-      routes: {
-        '/login': (BuildContext context) => LoginPage(),
-      },
     );
   }
 }
---
+++ lib/providers/authentication.dart
@@ -0,0 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_firestore_chatting_practice/services/database_service.dart';
+import 'package:flutter_firestore_chatting_practice/services/navigation_services.dart';
+import 'package:get_it/get_it.dart';
+
+class AuthenticationProvider extends ChangeNotifier {
+  late final FirebaseAuth _auth;
+  late final NavigationService _navigationService;
+  late final DatabaseService _databaseService;
+  AuthenticationProvider() {
+    _auth = FirebaseAuth.instance;
+    _navigationService = GetIt.instance.get<NavigationService>();
+    _databaseService = GetIt.instance.get<DatabaseService>();
+  }
+}
