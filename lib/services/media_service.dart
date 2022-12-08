4d5ac3ae1496db5f1b73617e028410c0c9761964
BeomYoonHeo <gqd0512@naver.com>
2022-12-08 22:49:11 +0900

    filepicker로 이미지 업데이트 시키기 완료

--- lib/services/media_service.dart
+++ lib/services/media_service.dart
@@ -2,6 +2,7 @@ import 'package:file_picker/file_picker.dart';
 
 class MediaService {
   MediaService() {}
+
   Future<PlatformFile?> pickImageFromLibrary() async {
     FilePickerResult? _result =
         await FilePicker.platform.pickFiles(type: FileType.image);
